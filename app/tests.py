7c236b82308ae6746e66a974b592e7d11fc9bf50
Samarth Dhruva <samarthdhruva@utexas.edu>
2016-06-30 18:00:55 -0500

    Added tests, put models inside app folder

---
+++ app/tests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+from unittest import Main, TestCase
+import models
+
+
+class ModelUnitTests(TestCase):
+
+	def setUp(self):
+		db.create_all()
+
+	def artist_model_test_1(self):
+		name = "Kanye West"
+		image_url = "http://cdn.uinterview.com/wp-content/uploads/2016/02/news-kanye-west.jpg"
+		image = Image(500, image_url, 500)
+		genres = ["Rap", "Hip Hop"]
+		popularity = 50
+		top_tracks = ["Blood On The Leaves", "Saint Pablo"]
+		test_artist = Artist(name, image, genres, popularity, top_tracks, spotify_url)
+        expect = ""
+		assertEquals(expect, test_artist.__repr__())
+
+#http://flask-sqlalchemy.pocoo.org/2.1/quickstart/
+
+	# def artist_model_test_1(self):
+	# 	name = "Kanye West"
+	# 	image_url = "http://cdn.uinterview.com/wp-content/uploads/2016/02/news-kanye-west.jpg"
+	# 	image = Image(500, image_url, 500)
+	# 	genres = ["Rap", "Hip Hop"]
+	# 	popularity = 50
+	# 	top_tracks = ["Blood On The Leaves", "Saint Pablo"]
+	# 	test_artist = Artist(name, image, genres, popularity, top_tracks, spotify_url)
+ #        expect = "Artist(name=%r, image=%r, genre=%r, popularity=%r, topTracks=%r, spotifyUrl=%r)" % (
+ #                    name,
+ #                    image,
+ #                    genres,
+ #                    popularity,
+ #                    top_tracks,
+ #               		spotify_url  
+ #                )
+	# 	assertEquals(expect, test_artist.__repr__())
