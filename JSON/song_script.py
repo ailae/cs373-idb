c48c2e309924e254fd3efbbfb4878ad37d97fc25
bratxdy <bratxdy@utexas.edu>
2016-07-08 14:31:14 -0500

    Updated the songs txt file with all the songs.

--- JSON/song_script.py
+++ JSON/song_script.py
@@ -21,9 +21,9 @@ while True :
 	else :
 		rank = line_array[0]
 		song = line_array[1]
-		artist = line_array[2]
+		artist = line_array[2].split('featuring')[0].split(',')[0]
 		
-		url = 'https://api.spotify.com/v1/search?type=track&q=track:'+song+' artist:'+artist+' year:'+str(year)+'&limit=1'
+		url = 'https://api.spotify.com/v1/search?type=track&q=track:'+song+' artist:'+artist+'&limit=1'
 		url = url.replace('%', '%25')
 		url = url.replace(' ', '%20')
 		
@@ -42,18 +42,19 @@ while True :
 		
 		song = items[0]
 		our_song_dict = dict()
-		our_song_dict['name'] = song['name']
-		our_song_dict['artist'] = song['artists'][0]['id']
-		our_song_dict['album'] = song['album']['name']
+		our_song_dict['song_id'] = song['id']
+		our_song_dict['song_name'] = song['name']
+		our_song_dict['artist_name'] = song['artists'][0]['name']
+		our_song_dict['artist_id'] = song['artists'][0]['id']
+		our_song_dict['album_name'] = song['album']['name']
 		our_song_dict['explicit'] = song['explicit']
 		our_song_dict['popularity'] = song['popularity']
-		our_song_dict['spotify_url'] = song['uri']
 		song_dicts += [our_song_dict]
 		
 	if not line :
 		break
 
-f = open('songs.json', 'w')
+f = open('songs.txt', 'w')
 f.write(str(song_dicts))
 f.close()
 
