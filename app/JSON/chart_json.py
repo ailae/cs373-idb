f675a3eec5d0e4d9e55858142de7acb98863c419
Kirk Johnson <kirk.johnson@utexas.edu>
2016-07-11 17:27:25 -0500

    renamed travis.yml

--- app/JSON/chart_json.py
+++ app/JSON/chart_json.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python
 
-charts = open('')
+charts = open('all_billboards_charts.txt', 'r')
 years = list()
-while True :
-    line = raw_input()
-    if not line :
-        break
-    
+for line in charts
     line_array = line.split('\t')
-    
-    year_dict = dict()
+    if len(line_array) == 1:
+        year_dict = dict()
     year_dict['year'] = line_array[0]
     year_dict['top_album_name'] = line_array[1]
     year_dict['top_album_id'] = line_array[3]
